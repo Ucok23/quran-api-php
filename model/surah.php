b73665d32c4686b70e4d2749d6f9a9c278830819
ucok23 <ulubis98@gmail.com>
2022-01-17 20:31:41 +0700

    updating surah handler

--- model/surah.php
+++ model/surah.php
@@ -1,11 +1,22 @@
 <?php
 
-class Surah {
-    
+class Surah {  
     function __construct($_quran_file) {
         $quran_file = file_get_contents($_quran_file);
         $quran_json = json_decode($quran_file, true)["data"];
         $this->quran = $quran_json;
+    }
+
+    function _checkSurah($_surah) {
+        if ($_surah < 1 || $_surah > 114) {
+            return false;
+        }
+    }
+
+    function _checkAyah($_surah, $_ayah) {
+        if ($_ayah < 0 || $_ayah > $this->quran[$_surah]["numberOfVerses"]) {
+            return false;
+        }
     }
 
     function getAllSurah() {
@@ -19,10 +30,15 @@ class Surah {
     }
 
     function getSurah($_surah) {
-        if ($_surah < 0 || $_surah > 114) {
-            return;
-        }
+        $this->_checkSurah($_surah);
         $surah = $this->quran[$_surah - 1];
         return $surah;
+    }
+
+    function getAyahFromSurah($_surah, $_ayah) {
+        $surah = $this->getSurah($_surah);
+        $this->_checkAyah($surah, $_ayah);
+        $ayah = $surah["verses"][$_ayah - 1];
+        return $ayah;
     }
 }
