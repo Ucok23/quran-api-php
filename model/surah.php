395ff80872e54f542e24c0bfed1b8c79d9b516ac
ucok23 <ulubis98@gmail.com>
2022-01-18 20:21:39 +0700

    make private method

--- model/surah.php
+++ model/surah.php
@@ -7,13 +7,13 @@ class Surah {
         $this->quran = $quran_json["data"];
     }
 
-    function _checkSurah($_surah) {
+    private function _checkSurah($_surah) {
         if ($_surah < 1 || $_surah > 114) {
             return false;
         }
     }
 
-    function _checkAyah($_surah, $_ayah) {
+    private function _checkAyah($_surah, $_ayah) {
         if ($_ayah < 0 || $_ayah > $this->quran[$_surah -1]["numberOfVerses"]) {
             return false;
         }
